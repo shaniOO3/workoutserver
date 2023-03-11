00f4851d4d9b1a8310f36124f70fa63d6aa0eb4c
shaniOO3 <69424936+shaniOO3@users.noreply.github.com>
2023-03-11 18:53:32 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+const workoutRoutes = require("./routes/workouts");
+
+const app = express();
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+app.use("/api/workouts", workoutRoutes);
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log("connected to db & listening on port", process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log("sorry oops", error);
+  });
+
+// app.listen(process.env.PORT, () => {
+//   console.log("listening on port 4000");
+// });
